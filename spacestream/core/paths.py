d7d6ca5e791be783353f0d2dc6863bed1d1c7728
Dawn Finzi <rebecca.d.finzi@gmail.com>
2023-12-09 18:48:46 -0800

    3D

--- spacestream/core/paths.py
+++ spacestream/core/paths.py
@@ -34,7 +34,8 @@ RESULTS_PATH = (
 
 # not included in general release due to size, inquire for access
 BETA_PATH = "/oak/stanford/groups/kalanit/biac2/kgs/projects/Dawn/NSD/local_data/processed/organized_betas/"
-STIM_PATH = "/oak/stanford/groups/kalanit/biac2/kgs/projects/Dawn/NSD/data/nsddata_stimuli/stimuli/nsd/"  # available through NSD release
+NSDDATA_PATH = "/oak/stanford/groups/kalanit/biac2/kgs/projects/Dawn/NSD/data/"
+STIM_PATH = NSDDATA_PATH + "nsddata_stimuli/stimuli/nsd/"  # available through NSD release
 HVM_PATH = "/oak/stanford/groups/kalanit/biac2/kgs/ml_datasets/hvm/"
 SINE_GRATING_PATH = (
     "/oak/stanford/groups/kalanit/biac2/kgs/ml_datasets/sine_grating_images_20190507"
